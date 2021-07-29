84827ff540b786ec0b9bda13d72a4a2d34993383
newt <ysti2019.win2020.st@gmail.com>
2021-07-30 00:01:42 +0900

    update: トップページに説明文を追加

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h2>Welcome web app</h2>
+    <p>
+      [caution!] This is beta version. You'll catch some bugs. I Welcome your
+      unkown issue's report
+    </p>
     <h3>Recent Post List</h3>
     <a href="/post">here</a>
     <h3>user data</h3>
@@ -9,6 +13,14 @@
     <a href="/getlog">here</a>
     <h3>search</h3>
     <a href="/search">here</a>
+    <h3>more info</h3>
+    <a
+      href="https://newt-house.notion.site/SHIP-Assistant-Project-Note-c599cf2125364a80920b0447f76d717a"
+      target="_blank"
+      >introduce functions, known bugs, fixes, later release schedule, and more
+    </a>
+    <h3>twitter</h3>
+    <a href="https://twitter.com/ShipNotify" target="_blank">here</a>
   </div>
 </template>
 
