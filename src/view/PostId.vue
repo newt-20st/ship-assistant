5751bcec5a994ee70508132ae0fe85e7bf7ae758
newt <ysti2019.win2020.st@gmail.com>
2021-09-01 11:09:17 +0900

    add: highSchoolNewsでpdfが表示されるように

--- src/view/PostId.vue
+++ src/view/PostId.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="post">
     <b-breadcrumb :items="path"></b-breadcrumb>
-    <h2>{{ rows[0].value }}</h2>
-    <table>
-      <tbody>
-        <tr v-for="row in rows" v-bind:key="row.id">
-          <th>{{ row.key }}</th>
-          <td>{{ row.value }}</td>
-        </tr>
-      </tbody>
-    </table>
-    <div v-show="file.name.length != 0">
-      <h3>Files</h3>
-      <ul>
-        <li v-for="(name, i) in file.name" :key="name">
-          <a :href="file.link[i]" target="_blank">{{ name }}</a>
-        </li>
-      </ul>
+    <div class="container-xxl">
+      <h2>{{ rows[0].value }}</h2>
+      <div class="row">
+        <div class="col-lg-8">
+          <table>
+            <tbody>
+              <tr v-for="row in rows" v-bind:key="row.id">
+                <th>{{ row.key }}</th>
+                <td>{{ row.value }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="col-lg-4" v-show="file.name.length != 0">
+          <h3>Files</h3>
+          <ul>
+            <li v-for="(name, i) in file.name" :key="name">
+              <a :href="file.link[i]" target="_blank">{{ name }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -100,11 +106,16 @@ export default {
             if (this.rows[i].key == "channel") {
               this.rows[i].value =
                 this.$store.state.channelData[getData[this.rows[i].key]].name;
+            } else if (
+              this.rows[i].key == "folder" &&
+              getData[this.rows[i].key] == ""
+            ) {
+              this.rows[i].value = "(root)";
             } else {
               this.rows[i].value = getData[this.rows[i].key];
             }
           }
-          if (getData.channel == "highCon" || getData.channel == "highStudy") {
+          if (getData.channel.indexOf("high") != -1) {
             const storage = firebase.storage();
             console.log(getData.link.length);
             for (let i = 0; i < getData.link.length; i++) {
